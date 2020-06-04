c531d44415a76e0646b494855491c7b59f7c3c1f
Rok Garbas <rok@garbas.si>
2020-06-05 00:51:30 +0200

    adding a footer and a thank you to bonsai

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ require("./index.scss");
 
 const {Elm} = require('./Main');
 
-Elm.Main.init({flags: {
-  elasticsearchMappingSchemaVersion: process.env.ELASTICSEARCH_MAPPING_SCHEMA_VERSION || 2,
-  elasticsearchUrl: process.env.ELASTICSEARCH_URL || 'https://nixos-search-5886075189.us-east-1.bonsaisearch.net:443',
-  elasticsearchUsername : process.env.ELASTICSEARCH_USERNAME || 'z3ZFJ6y2mR',
-  elasticsearchPassword : process.env.ELASTICSEARCH_PASSWORD || 'ds8CEvALPf9pui7XG'
-}});
+Elm.Main.init({
+  flags: {
+    elasticsearchMappingSchemaVersion: process.env.ELASTICSEARCH_MAPPING_SCHEMA_VERSION || 2,
+    elasticsearchUrl: process.env.ELASTICSEARCH_URL || 'https://nixos-search-5886075189.us-east-1.bonsaisearch.net:443',
+    elasticsearchUsername : process.env.ELASTICSEARCH_USERNAME || 'z3ZFJ6y2mR',
+    elasticsearchPassword : process.env.ELASTICSEARCH_PASSWORD || 'ds8CEvALPf9pui7XG'
+  }
+});
