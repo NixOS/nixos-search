ca730f6b1a7ab1c6036e926ff19f3c0929afb6c5
Marek Fajkus <marek.faj@gmail.com>
2020-10-27 17:24:07 +0100

    Percent encode query

--- src/Route.elm
+++ src/Route.elm
@@ -1,11 +1,13 @@
 module Route exposing (Route(..), fromUrl, href, replaceUrl)
 
+import Basics.Extra exposing (uncurry)
 import Browser.Navigation
 import Html
 import Html.Attributes
 import Url
+import Url.Builder as Builder exposing (QueryParameter)
 import Url.Parser exposing ((<?>))
-import Url.Parser.Query
+import Url.Parser.Query as Query
 
 
 
@@ -19,34 +21,35 @@ type Route
     | Options (Maybe String) (Maybe String) (Maybe String) (Maybe Int) (Maybe Int) (Maybe String)
 
 
+{-| Fixes issue with elm/url not properly escaping string
+-}
+queryString : String -> Query.Parser (Maybe String)
+queryString =
+    Query.map (Maybe.andThen Url.percentDecode) << Query.string
+
+
 parser : Url.Parser.Parser (Route -> msg) msg
 parser =
     Url.Parser.oneOf
-        [ Url.Parser.map
-            Home
-            Url.Parser.top
-        , Url.Parser.map
-            NotFound
-            (Url.Parser.s "not-found")
-        , Url.Parser.map
-            Packages
+        [ Url.Parser.map Home Url.Parser.top
+        , Url.Parser.map NotFound (Url.Parser.s "not-found")
+        , Url.Parser.map Packages
             (Url.Parser.s "packages"
-                <?> Url.Parser.Query.string "channel"
-                <?> Url.Parser.Query.string "query"
-                <?> Url.Parser.Query.string "show"
-                <?> Url.Parser.Query.int "from"
-                <?> Url.Parser.Query.int "size"
-                <?> Url.Parser.Query.string "sort"
+                <?> queryString "channel"
+                <?> queryString "query"
+                <?> queryString "show"
+                <?> Query.int "from"
+                <?> Query.int "size"
+                <?> queryString "sort"
             )
-        , Url.Parser.map
-            Options
+        , Url.Parser.map Options
             (Url.Parser.s "options"
-                <?> Url.Parser.Query.string "channel"
-                <?> Url.Parser.Query.string "query"
-                <?> Url.Parser.Query.string "show"
-                <?> Url.Parser.Query.int "from"
-                <?> Url.Parser.Query.int "size"
-                <?> Url.Parser.Query.string "sort"
+                <?> queryString "channel"
+                <?> queryString "query"
+                <?> queryString "show"
+                <?> Query.int "from"
+                <?> Query.int "size"
+                <?> queryString "sort"
             )
         ]
 
@@ -79,41 +82,64 @@ fromUrl url =
 
 
 routeToString : Route -> String
-routeToString page =
-    let
-        ( path, query ) =
-            routeToPieces page
-    in
-    "/" ++ String.join "/" path ++ "?" ++ String.join "&" (List.filterMap Basics.identity query)
+routeToString =
+    uncurry Builder.absolute << routeToPieces
 
 
-routeToPieces : Route -> ( List String, List (Maybe String) )
+{-| Fixes issue with elm/url not properly escaping string
+-}
+builderString : String -> String -> QueryParameter
+builderString name =
+    Builder.string name << Url.percentEncode
+
+
+routeToPieces : Route -> ( List String, List QueryParameter )
 routeToPieces page =
-    case page of
-        Home ->
-            ( [], [] )
+    let
+        channelQ =
+            Maybe.map (builderString "channel")
 
-        NotFound ->
-            ( [ "not-found" ], [] )
+        queryQ =
+            Maybe.map (builderString "query")
 
-        Packages channel query show from size sort ->
-            ( [ "packages" ]
-            , [ channel
-              , query
-              , show
-              , Maybe.map String.fromInt from
-              , Maybe.map String.fromInt size
-              , sort
-              ]
-            )
+        showQ =
+            Maybe.map (builderString "show")
 
-        Options channel query show from size sort ->
-            ( [ "options" ]
-            , [ channel
-              , query
-              , show
-              , Maybe.map String.fromInt from
-              , Maybe.map String.fromInt size
-              , sort
-              ]
-            )
+        fromQ =
+            Maybe.map (Builder.int "from")
+
+        sizeQ =
+            Maybe.map (Builder.int "size")
+
+        sortQ =
+            Maybe.map (builderString "sort")
+    in
+    Tuple.mapSecond (List.filterMap identity) <|
+        case page of
+            Home ->
+                ( [], [] )
+
+            NotFound ->
+                ( [ "not-found" ], [] )
+
+            Packages channel query show from size sort ->
+                ( [ "packages" ]
+                , [ channelQ channel
+                  , queryQ query
+                  , showQ show
+                  , fromQ from
+                  , sizeQ size
+                  , sortQ sort
+                  ]
+                )
+
+            Options channel query show from size sort ->
+                ( [ "options" ]
+                , [ channelQ channel
+                  , queryQ query
+                  , showQ show
+                  , fromQ from
+                  , sizeQ size
+                  , sortQ sort
+                  ]
+                )
