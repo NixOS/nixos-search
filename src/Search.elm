ff5c148c9fd86319f26b02cfff4c716764b65a3a
Yannik Sander <me@ysndr.de>
2021-06-14 13:41:06 +0200

    Remove Flakes Channel

--- src/Search.elm
+++ src/Search.elm
@@ -373,7 +373,6 @@ type Channel
     = Unstable
     | Release_20_09
     | Release_21_05
-    | Flakes
 
 
 {-| TODO: we should consider using more dynamic approach here
@@ -404,9 +403,6 @@ channelDetails channel =
         Release_21_05 ->
             ChannelDetails "21.05" "21.05" "nixos/release-21.05" "nixos-21.05"
 
-        Flakes ->
-            ChannelDetails "flakes" "Flakes" "Nix Flakes" "--"
-
 channelFromId : String -> Maybe Channel
 channelFromId channel_id =
     case channel_id of
@@ -419,8 +415,6 @@ channelFromId channel_id =
         "21.05" ->
             Just Release_21_05
 
-        "flakes" ->
-            Just Flakes
         _ ->
             Nothing
 
@@ -436,7 +430,6 @@ channels =
     [ "20.09"
     , "21.05"
     , "unstable"
-    , "flakes"
     ]
 
 
