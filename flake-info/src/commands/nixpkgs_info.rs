f8a1d7a0f2ac3ecdb9b43ab2762ac333915052c4
Yannik Sander <me@ysndr.de>
2021-06-16 15:33:28 +0200

    Format nixpkgs command runner

--- flake-info/src/commands/nixpkgs_info.rs
+++ flake-info/src/commands/nixpkgs_info.rs
@@ -1,18 +1,15 @@
-use std::{collections::HashMap, fmt::Display, fs::File};
 use anyhow::{Context, Result};
 use std::io::Write;
-
+use std::{collections::HashMap, fmt::Display, fs::File};
 
 use command_run::Command;
 use log::debug;
 
-use crate::data::import::{NixpkgsEntry, Package};
-
+use crate::data::import::{NixpkgsEntry, OneOrMany, Package};
 
 const NIXPKGS_SCRIPT: &str = include_str!("packages-config.nix");
 
-
-pub fn get_nixpkgs_info<T: AsRef<str> + Display>(nixpkgs_flake_ref: T) ->  Result<Vec<NixpkgsEntry>> {
+pub fn get_nixpkgs_info<T: AsRef<str> + Display>(nixpkgs_channel: T) -> Result<Vec<NixpkgsEntry>> {
     let script_dir = tempfile::tempdir()?;
     let script_path = script_dir.path().join("packages-config.nix");
     writeln!(File::create(&script_path)?, "{}", NIXPKGS_SCRIPT)?;
@@ -20,21 +17,33 @@ pub fn get_nixpkgs_info<T: AsRef<str> + Display>(nixpkgs_flake_ref: T) ->  Resul
     let mut command = Command::new("nix-env");
     let command = command.enable_capture();
     let command = command.add_args(&[
-    "-f", "<nixpkgs>",
-    "-I", nixpkgs_flake_ref.as_ref(),
-    "--arg", "config", format!("import {}", script_path.to_str().unwrap()).as_str(),
-    "-qa",
-    "--json"]);
+        "-f",
+        "<nixpkgs>",
+        "-I", nixpkgs_channel.as_ref(),
+        "--arg",
+        "config",
+        format!("import {}", script_path.to_str().unwrap()).as_str(),
+        "-qa",
+        "--json",
+    ]);
 
     let parsed: Result<Vec<NixpkgsEntry>> = command
         .run()
-        .with_context(|| format!("Failed to gather information about nixpkgs {}", nixpkgs_flake_ref.as_ref()))
+        .with_context(|| {
+            format!(
+                "Failed to gather information about nixpkgs {}",
+                nixpkgs_channel.as_ref()
+            )
+        })
         .and_then(|o| {
             debug!("stderr: {}", o.stderr_string_lossy());
-            let attr_set: HashMap<String, Package> = serde_json::de::from_str(&o.stdout_string_lossy())?;
-            Ok(attr_set.into_iter().map(|(attribute, package)| NixpkgsEntry{attribute, package}).collect())
+            let attr_set: HashMap<String, OneOrMany<Package>> =
+                serde_json::de::from_str(&o.stdout_string_lossy())?;
+            Ok(attr_set
+                .into_iter()
+                .map(|(attribute, package)| NixpkgsEntry { attribute, package: package.into_list().get(0).unwrap().clone() })
+                .collect())
         });
 
     parsed
-
 }
