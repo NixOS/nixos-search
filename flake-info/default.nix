2f2ad45ee8990a8585f69a1588443a35a09875c5
Yannik Sander <me@ysndr.de>
2021-06-17 13:16:58 +0200

    Expose flake-info as derivation

--- flake-info/default.nix
+++ flake-info/default.nix
@@ -1,25 +1,15 @@
-{ pkgs ? import <nixpkgs> { } }:
-
-let nix = pkgs.nixUnstable.override {
-    patches = [(
-        pkgs.fetchpatch {
-    name = "1230.patch";
-    url = "https://patch-diff.githubusercontent.com/raw/NixOS/nix/pull/1230.patch";
-    sha256 = "sha256-iL4m8j0ootuWDnKfrdme4vIkCHTTtbxQxnf6VLdIn6o=";
-    })];
-};
+{ pkgs ? import <nixpkgs> { } }: with pkgs;
 
 
-in
-nix
 
-# pkgs.rustPlatform.buildRustPackage rec {
-#     name = "flake-info";
-#     src = ./flake-info;
-#     cargoSha256 = "sha256-JPMthz+z9qNG6CK8F+P/iG77VCd0X7/dyS3YpKeGskc=";
-#     buildInputs = [ ] ++ optional pkgs.lib.isDarwin [pkgs.libiconv pkgs.darwin.apple_sdk.frameworks.Security];
-#     checkFlags = [
-#         "--skip elastic::tests"
-#         "--skip nix_gc::tests"
-#     ];
-# }
+
+rustPlatform.buildRustPackage rec {
+    name = "flake-info";
+    src = ./.;
+    cargoSha256 = "sha256-JPMthz+z9qNG6CK8F+P/iG77VCd0X7/dyS3YpKeGskc=";
+    buildInputs = [ ] ++ lib.optional pkgs.stdenv.isDarwin [libiconv darwin.apple_sdk.frameworks.Security];
+    checkFlags = [
+        "--skip elastic::tests"
+        "--skip nix_gc::tests"
+    ];
+}
