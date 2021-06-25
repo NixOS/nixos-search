ee2a01b6c713124a2f7f77ac3d2bebd48b2898c9
Yannik Sander <me@ysndr.de>
2021-06-25 15:03:27 +0200

    Combine Package and Option Search

--- src/Page/Flakes.elm
+++ src/Page/Flakes.elm
@@ -1,50 +1,63 @@
-module Page.Flakes exposing (Model, Msg, init, update, view)
+module Page.Flakes exposing (Model(..), Msg(..), init, makeRequest, update, view)
 
 import Browser.Navigation
-import Html exposing (Html, a, code, div, li, pre, strong, text, ul)
+import Html exposing (Html, a, code, div, li, nav, pre, strong, text, ul)
 import Html.Attributes exposing (class, classList, href, target)
 import Html.Events exposing (onClick)
 import Html.Parser
 import Html.Parser.Util
-import Json.Decode
-import Route
+import Http exposing (Body)
+import Json.Decode exposing (Decoder)
+import Page.Options
+import Page.Packages exposing (Msg(..))
+import Route exposing (Route(..), SearchArgs, SearchType(..))
 import Search
+import View.Components
 
 
 
 -- MODEL
-type alias Model =
-    Search.Model ResultItemSource ResultAggregations
 
 
-type alias ResultItemSource =
-    { name : String
-    , description : Maybe String
-    , type_ : Maybe String
-    , default : Maybe String
-    , example : Maybe String
-    , source : Maybe String
-    }
-
-
-type alias ResultAggregations =
-    { all : AggregationsAll
-    }
-
-
-type alias AggregationsAll =
-    { doc_count : Int
-    }
+type Model
+    = OptionModel Page.Options.Model
+    | PackagesModel Page.Packages.Model
 
 
 init : Route.SearchArgs -> Maybe Model -> ( Model, Cmd Msg )
 init searchArgs model =
     let
+        _ =
+            Debug.log "Flakes" "init"
+
+        --  init with respective module or with packages by default
+        mapEitherModel m =
+            case m of
+                OptionModel model_ ->
+                    Tuple.mapBoth OptionModel (Cmd.map OptionsMsg) <| Page.Options.init searchArgs <| Just model_
+
+                PackagesModel model_ ->
+                    Tuple.mapBoth PackagesModel (Cmd.map PackagesMsg) <| Page.Packages.init searchArgs <| Just model_
+
+        default =
+            case Maybe.withDefault PackageSearch searchArgs.type_ of
+                PackageSearch ->
+                    Tuple.mapBoth PackagesModel (Cmd.map PackagesMsg) <| Page.Packages.init searchArgs Nothing
+
+                OptionSearch ->
+                    Tuple.mapBoth OptionModel (Cmd.map OptionsMsg) <| Page.Options.init searchArgs Nothing
+
+                FlakeSearch ->
+                    Debug.todo "branch 'FlakeSearch' not implemented"
+
         ( newModel, newCmd ) =
-            Search.init searchArgs model
+            Maybe.withDefault default <| Maybe.map mapEitherModel model
+
+        _ =
+            Debug.log "mapped Model" <| Maybe.map mapEitherModel model
     in
     ( newModel
-    , Cmd.map SearchMsg newCmd
+    , newCmd
     )
 
 
@@ -53,7 +66,8 @@ init searchArgs model =
 
 
 type Msg
-    = SearchMsg (Search.Msg ResultItemSource ResultAggregations)
+    = OptionsMsg Page.Options.Msg
+    | PackagesMsg Page.Packages.Msg
 
 
 update :
@@ -62,17 +76,45 @@ update :
     -> Model
     -> ( Model, Cmd Msg )
 update navKey msg model =
-    case msg of
-        SearchMsg subMsg ->
-            let
-                ( newModel, newCmd ) =
-                    Search.update
-                        Route.Flakes
-                        navKey
-                        subMsg
-                        model
-            in
-            ( newModel, Cmd.map SearchMsg newCmd )
+    let
+        _ =
+            Debug.log "Flake update" ( msg, model )
+    in
+    case ( msg, model ) of
+        ( OptionsMsg msg_, OptionModel model_ ) ->
+            case msg_ of
+                Page.Options.SearchMsg subMsg ->
+                    let
+                        _ =
+                            Debug.log "update - options"
+
+                        ( newModel, newCmd ) =
+                            Search.update
+                                Route.Flakes
+                                navKey
+                                subMsg
+                                model_
+                    in
+                    ( newModel, Cmd.map Page.Options.SearchMsg newCmd ) |> Tuple.mapBoth OptionModel (Cmd.map OptionsMsg)
+
+        ( PackagesMsg msg_, PackagesModel model_ ) ->
+            case msg_ of
+                Page.Packages.SearchMsg subMsg ->
+                    let
+                        _ =
+                            Debug.log "Flakes" "update - packages"
+
+                        ( newModel, newCmd ) =
+                            Search.update
+                                Route.Flakes
+                                navKey
+                                subMsg
+                                model_
+                    in
+                    ( newModel, Cmd.map Page.Packages.SearchMsg newCmd ) |> Tuple.mapBoth PackagesModel (Cmd.map PackagesMsg)
+
+        _ ->
+            ( model, Cmd.none )
 
 
 
@@ -81,151 +123,25 @@ update navKey msg model =
 
 view : Model -> Html Msg
 view model =
-    Search.view { toRoute = Route.Flakes, categoryName = "options" }
-        [ text "Search more than "
-        , strong [] [ text "10 000 options" ]
-        ]
-        model
-        viewSuccess
-        viewBuckets
-        SearchMsg
-
-
-viewBuckets :
-    Maybe String
-    -> Search.SearchResult ResultItemSource ResultAggregations
-    -> List (Html Msg)
-viewBuckets _ _ =
-    []
-
-
-viewSuccess :
-    String
-    -> Bool
-    -> Maybe String
-    -> List (Search.ResultItem ResultItemSource)
-    -> Html Msg
-viewSuccess channel showNixOSDetails show hits =
-    ul []
-        (List.map
-            (viewResultItem channel showNixOSDetails show)
-            hits
-        )
-
-
-viewResultItem :
-    String
-    -> Bool
-    -> Maybe String
-    -> Search.ResultItem ResultItemSource
-    -> Html Msg
-viewResultItem channel _ show item =
     let
-        showHtml value =
-            case Html.Parser.run value of
-                Ok nodes ->
-                    Html.Parser.Util.toVirtualDom nodes
+        mkBody =
+            View.Components.body { toRoute = Route.Flakes, categoryName = "options" }
+                [ text "Search more than "
+                , strong [] [ text "10 000 options" ]
+                ]
 
-                Err _ ->
-                    []
+        _ =
+            Debug.log "flakes view renders" model
 
-        default =
-            "Not given"
+        body =
+            case model of
+                OptionModel model_ ->
+                    Html.map OptionsMsg <| mkBody model_ Page.Options.viewSuccess Page.Options.viewBuckets Page.Options.SearchMsg
 
-        asPre value =
-            pre [] [ text value ]
-
-        asPreCode value =
-            div [] [ pre [] [ code [ class "code-block" ] [ text value ] ] ]
-
-        githubUrlPrefix branch =
-            "https://github.com/NixOS/nixpkgs/blob/" ++ branch ++ "/"
-
-        cleanPosition value =
-            if String.startsWith "source/" value then
-                String.dropLeft 7 value
-
-            else
-                value
-
-        asGithubLink value =
-            case Search.channelDetailsFromId channel of
-                Just channelDetails ->
-                    a
-                        [ href <| githubUrlPrefix channelDetails.branch ++ (value |> String.replace ":" "#L")
-                        , target "_blank"
-                        ]
-                        [ text value ]
-
-                Nothing ->
-                    text <| cleanPosition value
-
-        withEmpty wrapWith maybe =
-            case maybe of
-                Nothing ->
-                    asPre default
-
-                Just "" ->
-                    asPre default
-
-                Just value ->
-                    wrapWith value
-
-        wrapped wrapWith value =
-            case value of
-                "" ->
-                    wrapWith <| "\"" ++ value ++ "\""
-
-                _ ->
-                    wrapWith value
-
-        showDetails =
-            if Just item.source.name == show then
-                div [ Html.Attributes.map SearchMsg Search.trapClick ]
-                    [ div [] [ text "Name" ]
-                    , div [] [ wrapped asPreCode item.source.name ]
-                    , div [] [ text "Description" ]
-                    , div [] <|
-                        (item.source.description
-                            |> Maybe.map showHtml
-                            |> Maybe.withDefault []
-                        )
-                    , div [] [ text "Default value" ]
-                    , div [] [ withEmpty (wrapped asPreCode) item.source.default ]
-                    , div [] [ text "Type" ]
-                    , div [] [ withEmpty asPre item.source.type_ ]
-                    , div [] [ text "Example" ]
-                    , div [] [ withEmpty (wrapped asPreCode) item.source.example ]
-                    , div [] [ text "Declared in" ]
-                    , div [] [ withEmpty asGithubLink item.source.source ]
-                    ]
-                    |> Just
-
-            else
-                Nothing
-
-        toggle =
-            SearchMsg (Search.ShowDetails item.source.name)
-
-        isOpen =
-            Just item.source.name == show
+                PackagesModel model_ ->
+                    Html.map PackagesMsg <| mkBody model_ Page.Packages.viewSuccess Page.Packages.viewBuckets Page.Packages.SearchMsg
     in
-    li
-        [ class "option"
-        , classList [ ( "opened", isOpen ) ]
-        , Search.elementId item.source.name
-        ]
-    <|
-        List.filterMap identity
-            [ Just <|
-                Html.a
-                    [ class "search-result-button"
-                    , onClick toggle
-                    , href ""
-                    ]
-                    [ text item.source.name ]
-            , showDetails
-            ]
+    body
 
 
 
@@ -234,6 +150,7 @@ viewResultItem channel _ show item =
 
 makeRequest :
     Search.Options
+    -> SearchType
     -> String
     -> String
     -> Int
@@ -241,55 +158,48 @@ makeRequest :
     -> Maybe String
     -> Search.Sort
     -> Cmd Msg
-makeRequest options channel query from size _ sort =
-    Search.makeRequest
-        (Search.makeRequestBody
-            (String.trim query)
-            from
-            size
-            sort
-            "option"
-            "option_name"
-            []
-            []
-            []
-            "option_name"
-            [ ( "option_name", 6.0 )
-            , ( "option_name_query", 3.0 )
-            , ( "option_description", 1.0 )
-            ]
-        )
-        channel
-        decodeResultItemSource
-        decodeResultAggregations
-        options
-        Search.QueryResponse
-        (Just "query-options")
-        |> Cmd.map SearchMsg
+makeRequest options searchType channel query from size maybeBuckets sort =
+    let
+        cmd =
+            case searchType of
+                PackageSearch ->
+                    Search.makeRequest
+                        (makeRequestBody searchType query from size maybeBuckets sort)
+                        (String.fromInt options.mappingSchemaVersion ++ "-" ++ channel)
+                        Page.Packages.decodeResultItemSource
+                        Page.Packages.decodeResultAggregations
+                        options
+                        Search.QueryResponse
+                        (Just "query-packages")
+                        |> Cmd.map Page.Packages.SearchMsg
+                        |> Cmd.map PackagesMsg
+
+                OptionSearch ->
+                    Search.makeRequest
+                        (makeRequestBody searchType query from size maybeBuckets sort)
+                        (String.fromInt options.mappingSchemaVersion ++ "-" ++ channel)
+                        Page.Options.decodeResultItemSource
+                        Page.Options.decodeResultAggregations
+                        options
+                        Search.QueryResponse
+                        (Just "query-options")
+                        |> Cmd.map Page.Options.SearchMsg
+                        |> Cmd.map OptionsMsg
+
+                FlakeSearch ->
+                    Debug.todo "branch 'FlakeSearch' not implemented"
+    in
+    cmd
 
 
+makeRequestBody : SearchType -> String -> Int -> Int -> Maybe String -> Search.Sort -> Body
+makeRequestBody searchType query from size maybeBuckets sort =
+    case searchType of
+        OptionSearch ->
+            Page.Options.makeRequestBody query from size sort
 
--- JSON
+        PackageSearch ->
+            Page.Packages.makeRequestBody query from size maybeBuckets sort
 
-
-decodeResultItemSource : Json.Decode.Decoder ResultItemSource
-decodeResultItemSource =
-    Json.Decode.map6 ResultItemSource
-        (Json.Decode.field "option_name" Json.Decode.string)
-        (Json.Decode.field "option_description" (Json.Decode.nullable Json.Decode.string))
-        (Json.Decode.field "option_type" (Json.Decode.nullable Json.Decode.string))
-        (Json.Decode.field "option_default" (Json.Decode.nullable Json.Decode.string))
-        (Json.Decode.field "option_example" (Json.Decode.nullable Json.Decode.string))
-        (Json.Decode.field "option_source" (Json.Decode.nullable Json.Decode.string))
-
-
-decodeResultAggregations : Json.Decode.Decoder ResultAggregations
-decodeResultAggregations =
-    Json.Decode.map ResultAggregations
-        (Json.Decode.field "all" decodeResultAggregationsAll)
-
-
-decodeResultAggregationsAll : Json.Decode.Decoder AggregationsAll
-decodeResultAggregationsAll =
-    Json.Decode.map AggregationsAll
-        (Json.Decode.field "doc_count" Json.Decode.int)
+        FlakeSearch ->
+            Debug.todo "branch 'FlakeSearch' not implemented"
