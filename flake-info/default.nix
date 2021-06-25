ddaa063ad91b52cfc102509e7187ec51cb2ffee8
Yannik Sander <me@ysndr.de>
2021-06-25 15:08:54 +0200

    Add SSL dependency

--- flake-info/default.nix
+++ flake-info/default.nix
@@ -7,7 +7,7 @@ rustPlatform.buildRustPackage rec {
     name = "flake-info";
     src = ./.;
     cargoSha256 = "sha256-mrk5kHU/BGuij9ZrscgZolKDfWzFWfb9Ws4STl2VHpk=";
-    buildInputs = [ ] ++ lib.optional pkgs.stdenv.isDarwin [libiconv darwin.apple_sdk.frameworks.Security];
+    buildInputs = [ pkg-config openssl ] ++ lib.optional pkgs.stdenv.isDarwin [libiconv darwin.apple_sdk.frameworks.Security];
     checkFlags = [
         "--skip elastic::tests"
         "--skip nix_gc::tests"
