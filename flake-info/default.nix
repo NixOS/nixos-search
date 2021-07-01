321464dfe20d13916c95f3839f6fa3b1c3e23213
Yannik Sander <accounts@ysndr.de>
2021-07-01 12:48:45 +0200

    Cron jobs for nixpkgs using flake-info (#321)
    * Add nixpkgs cron job
    
    * More precise name for nixpkgs cron job
    
    * Add Flakes cron job
    
    * Read version from file and fix channel names
    
    * Correct file names for flake group import
    
    * Fix group command
    
    * Run new cron jobs on PR
    
    * Update Cargo sources
    
    * Add SSL dependency

--- flake-info/default.nix
+++ flake-info/default.nix
@@ -6,8 +6,8 @@
 rustPlatform.buildRustPackage rec {
     name = "flake-info";
     src = ./.;
-    cargoSha256 = "sha256-JPMthz+z9qNG6CK8F+P/iG77VCd0X7/dyS3YpKeGskc=";
-    buildInputs = [ ] ++ lib.optional pkgs.stdenv.isDarwin [libiconv darwin.apple_sdk.frameworks.Security];
+    cargoSha256 = "sha256-mrk5kHU/BGuij9ZrscgZolKDfWzFWfb9Ws4STl2VHpk=";
+    buildInputs = [ pkg-config openssl ] ++ lib.optional pkgs.stdenv.isDarwin [libiconv darwin.apple_sdk.frameworks.Security];
     checkFlags = [
         "--skip elastic::tests"
         "--skip nix_gc::tests"
