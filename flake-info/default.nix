0daba8c1bc94321578ec9ad74346bb9de0d9350d
Yannik Sander <me@ysndr.de>
2021-08-13 14:18:55 +0200

    Use cargo sha as prompted by github action

--- flake-info/default.nix
+++ flake-info/default.nix
@@ -6,7 +6,7 @@
 rustPlatform.buildRustPackage rec {
     name = "flake-info";
     src = ./.;
-    cargoSha256 = "sha256-qooAjbvdAcfBj5gm8kYbq1m8CZAcpz0KHROBV58lC+Q=";
+    cargoSha256 = "sha256-TA1WEvmOfnxQ+rRwkIPN1t4VPrDL6pq+WnPHVu2/CPE=";
     nativeBuildInputs = [ pkg-config ];
     buildInputs = [ openssl openssl.dev ] ++ lib.optional pkgs.stdenv.isDarwin [libiconv darwin.apple_sdk.frameworks.Security];
     checkFlags = [
