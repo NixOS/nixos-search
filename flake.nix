be2a7acab09d56a8e77556f4bd121876c455fdf7
binarycat <binarycat@envs.net>
2024-03-29 18:16:19 -0400

    add support for new meta.repository field

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   nixConfig.extra-substituters = [ "https://nixos-search.cachix.org" ];
   nixConfig.extra-trusted-public-keys = [ "nixos-search.cachix.org-1:1HV3YF8az4fywnH+pAd+CXFEdpTXtv9WpoivPi+H70o=" ];
 
-  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs?rev=e3652e0735fbec227f342712f180f4f21f0594f2";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.npmlock2nix.url = "github:nix-community/npmlock2nix";
   inputs.npmlock2nix.flake = false;
