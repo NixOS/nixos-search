2827f01b9a00960c2ff8fe96ea9a4b87ae8fa201
Tim Van Baak <tim.vanbaak@gmail.com>
2025-05-14 21:14:10 -0700

    Add NixOS configuration for an ephemeral OpenSearch vm for local development

--- flake.nix
+++ flake.nix
@@ -140,5 +140,35 @@
           devShell = warnToUpgradeNix devShells.default;
           defaultPackage = warnToUpgradeNix packages.default;
         }
-      );
+      ) // {
+        # Local testing VM configuration
+        nixosConfigurations.opensearch-vm = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            (import "${nixpkgs}/nixos/modules/virtualisation/qemu-vm.nix")
+            ({ lib, pkgs, ... }: {
+              system.stateVersion = "25.05";
+              services.getty.autologinUser = "root";
+              virtualisation = {
+                diskImage = null; # makes VM ephemeral
+                graphics = false;
+                forwardPorts = [ { guest.port = 9200; host.port = 9200; } ]; # expose :9200 from guest to host
+                memorySize = 1024 * 8;  # 8 GB
+              };
+              environment.systemPackages = [ pkgs.opensearch-cli ];
+              networking.firewall.allowedTCPPorts = [ 9200 ];
+              services.opensearch = {
+                enable = true;
+                settings = {
+                  "network.host" = "0.0.0.0";
+                  "http.cors.enabled" = "true";
+                  "http.cors.allow-origin" = "http://localhost:3000";
+                  "http.cors.allow-credentials" = "true";
+                  "http.cors.allow-headers" = "X-Requested-With,X-Auth-Token,Content-Type,Content-Length,Authorization";
+                };
+              };
+            })
+          ];
+        };
+      };
 }
