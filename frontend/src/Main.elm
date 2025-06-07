1293fa528a9fe05ce109aadb6889a29e903d5735
Arnout Engelen <arnout@bzzt.net>
2025-06-07 10:41:46 +0200

    deploy pride logo

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -405,7 +405,7 @@ view model =
                         [ div [ class "navbar-inner" ]
                             [ div [ class "container" ]
                                 [ a [ class "brand", href "https://nixos.org" ]
-                                    [ img [ alt "NixOS logo", src "/images/nix-logo.png", class "logo" ] []
+                                    [ img [ alt "NixOS logo", src "/images/nix-logo-pride.png", class "logo" ] []
                                     ]
                                 , div []
                                     [ ul [ class "nav pull-left" ]
