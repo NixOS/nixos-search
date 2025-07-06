a6f9f981a97c2aaca03d5c42d3ff4bf5e34060c7
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2025-07-06 14:49:45 +0200

    Apply simplifications suggested by elm-review

--- frontend/src/Search.elm
+++ frontend/src/Search.elm
@@ -258,9 +258,6 @@ decodeResolvedFlake =
                         _ ->
                             Nothing
 
-                url =
-                    resolved_.url
-
                 result =
                     case resolved_.type_ of
                         "github" ->
@@ -273,7 +270,7 @@ decodeResolvedFlake =
                             Maybe.map (\repoPath_ -> "https://sr.ht/" ++ repoPath_) repoPath
 
                         "git" ->
-                            url
+                            resolved_.url
 
                         _ ->
                             Nothing
@@ -519,12 +516,13 @@ pushUrl :
     -> Model a b
     -> ( Model a b, Cmd msg )
 pushUrl toRoute navKey model =
-    Tuple.pair model <|
-        if model.query == Nothing || model.query == Just "" then
-            Cmd.none
+    ( model
+    , if model.query == Nothing || model.query == Just "" then
+        Cmd.none
 
-        else
-            Browser.Navigation.pushUrl navKey <| createUrl toRoute model
+      else
+        Browser.Navigation.pushUrl navKey <| createUrl toRoute model
+    )
 
 
 createUrl :
@@ -816,15 +814,15 @@ viewResult nixosChannels outMsg toRoute categoryName model viewSuccess viewBucke
                 buckets =
                     viewBuckets model.buckets result
             in
-            if result.hits.total.value == 0 && List.length buckets == 0 then
+            if result.hits.total.value == 0 && List.isEmpty buckets then
                 div [ class "search-results" ]
                     [ ul [ class "search-sidebar" ] searchBuckets
                     , viewNoResults categoryName (Maybe.withDefault "" model.query)
                     ]
 
-            else if List.length buckets > 0 then
+            else if not (List.isEmpty buckets) then
                 div [ class "search-results" ]
-                    [ ul [ class "search-sidebar" ] <| List.append searchBuckets buckets
+                    [ ul [ class "search-sidebar" ] (searchBuckets ++ buckets)
                     , div []
                         (viewResults nixosChannels model result viewSuccess toRoute outMsg categoryName)
                     ]
@@ -992,7 +990,7 @@ viewChannels nixosChannels outMsg selectedChannel =
                                 ]
                             , onClick <| outMsg (ChannelChange channel.id)
                             ]
-                            (List.intersperse (text " ") ([ text channel.id ] ++ channelBadge channel.status))
+                            (List.intersperse (text " ") (text channel.id :: channelBadge channel.status))
                     )
                     nixosChannels
                 )
@@ -1039,9 +1037,6 @@ viewResults nixosChannels model result viewSuccess _ outMsg categoryName =
                  else
                     model.from + model.size
                 )
-
-        total =
-            String.fromInt result.hits.total.value
     in
     [ div []
         (List.append
@@ -1060,6 +1055,10 @@ viewResults nixosChannels model result viewSuccess _ outMsg categoryName =
                         ]
 
                      else
+                        let
+                            total =
+                                String.fromInt result.hits.total.value
+                        in
                         [ strong []
                             [ text total
                             , text " "
@@ -1247,13 +1246,12 @@ searchFields query mainField fields =
     let
         allFields =
             fields
-                |> List.map
+                |> List.concatMap
                     (\( field, score ) ->
                         [ field ++ "^" ++ String.fromFloat score
                         , field ++ ".*^" ++ String.fromFloat (score * 0.6)
                         ]
                     )
-                |> List.concat
 
         queryWordsWildCard =
             (String.replace "_" "-" query :: String.replace "-" "_" query :: queryWords)
