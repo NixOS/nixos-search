956ed0ebf3ca525383233f508176e65b7d77490e
Arnout Engelen <arnout@bzzt.net>
2025-11-04 09:15:38 +0100

    Revert "Split flake lists into groups (#1037)"
    This reverts commit 696ca385807c865c8d795046bdf6aef14f957ffb.

--- frontend/src/Page/Flakes.elm
+++ frontend/src/Page/Flakes.elm
@@ -162,7 +162,7 @@ view nixosChannels model =
             [ text "Search packages and options of "
             , strong []
                 [ a
-                    [ href "https://github.com/NixOS/nixos-search/tree/main/flakes" ]
+                    [ href "https://github.com/NixOS/nixos-search/blob/main/flakes/manual.toml" ]
                     [ text "public flakes" ]
                 ]
             ]
