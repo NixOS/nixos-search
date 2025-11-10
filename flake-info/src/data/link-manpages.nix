f14029b03ab263b855ce553a80a4e1b90d3d4a5a
dyegoaurelio <42411160+dyegoaurelio@users.noreply.github.com>
2025-11-10 12:36:34 -0300

    fix: using nixfmt instead of deprecated nixpkgs-fmt (#1090)
    I missed this detail when creating the config file

--- flake-info/src/data/link-manpages.nix
+++ flake-info/src/data/link-manpages.nix
@@ -9,8 +9,9 @@ pkgs.writeText "link-manpages.lua" ''
   ]]
 
   local manpage_urls = {
-  ${lib.concatStringsSep "\n" (lib.mapAttrsToList (man: url:
-    "  [${builtins.toJSON man}] = ${builtins.toJSON url},") manpageURLs)}
+  ${lib.concatStringsSep "\n" (
+    lib.mapAttrsToList (man: url: "  [${builtins.toJSON man}] = ${builtins.toJSON url},") manpageURLs
+  )}
   }
 
   traverse = 'topdown'
