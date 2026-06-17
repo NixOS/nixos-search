902e0f043047960edb3f623014fd03f0f728cd5f
Yohann Boniface <edhyjox@gmail.com>
2026-06-18 00:34:43 +0200

    build(frontend): modernize packaging (#1341)
    * build(frontend): reduce amount of vulnerable deps
    
    * build(frontend): get rid of npmlock2nix
    
    * build(frontend): move to package lock v3
    
    * build(frontend): rerun npm audit
    
    * build(frontend): more deps bumping

--- frontend/default.nix
+++ frontend/default.nix
@@ -1,48 +1,52 @@
 {
-  pkgs ? import <nixpkgs> { },
+  stdenvNoCC,
+  fetchNpmDeps,
+  elmPackages,
+  nodejs,
+  npmHooks,
+
   nixosChannels,
   version,
 }:
-pkgs.npmlock2nix.v1.build {
+stdenvNoCC.mkDerivation (finalAttrs: {
+  name = "frontend";
   src = ./.;
+
+  npmDeps = fetchNpmDeps {
+    pname = "npm-deps-${finalAttrs.name}";
+    inherit (finalAttrs) src;
+    hash = "sha256-sRe4+flCRBH9dSPrC52ZF238+RRe9qyFeuSDyT2lpd8=";
+  };
+
+  postConfigure = elmPackages.fetchElmDeps {
+    elmPackages = import ./elm-srcs.nix;
+    elmVersion = elmPackages.elm.version;
+    registryDat = ./registry.dat;
+  };
+
+  strictDeps = true;
+
+  env = {
+    ELASTICSEARCH_MAPPING_SCHEMA_VERSION = version;
+    NIXOS_CHANNELS = builtins.toJSON nixosChannels;
+  };
+
+  nativeBuildInputs = [
+    nodejs
+    npmHooks.npmConfigHook
+    npmHooks.npmBuildHook
+    npmHooks.npmInstallHook
+  ];
+
+  npmBuildScript = "prod";
+
   installPhase = ''
+    runHook preInstall
+
     mkdir $out
     cp -R dist/* $out/
     cp netlify.toml $out/
+
+    runHook postInstall
   '';
-  postConfigure = pkgs.elmPackages.fetchElmDeps {
-    elmPackages = import ./elm-srcs.nix;
-    elmVersion = pkgs.elmPackages.elm.version;
-    registryDat = ./registry.dat;
-  };
-  ELASTICSEARCH_MAPPING_SCHEMA_VERSION = version;
-  NIXOS_CHANNELS = builtins.toJSON nixosChannels;
-  buildCommands = [
-    "HOME=$PWD npm run prod"
-  ];
-  buildInputs =
-    (with pkgs; [
-      nodejs
-      elm2nix
-    ])
-    ++ (with pkgs.elmPackages; [
-      elm
-      elm-format
-      elm-language-server
-      elm-test
-    ]);
-  node_modules_attrs = {
-    sourceOverrides = {
-      elm =
-        sourceIngo: drv:
-        drv.overrideAttrs (old: {
-          postPatch = ''
-            sed -i -e "s|download(|//download(|" install.js
-            sed -i -e "s|request(|//request(|" download.js
-            sed -i -e "s|var version|return; var version|" download.js
-            cp ${pkgs.elmPackages.elm}/bin/elm bin/elm
-          '';
-        });
-    };
-  };
-}
+})
