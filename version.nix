c3babe1885d39132e01ae90aff3fd814fca7025a
Arnout Engelen <arnout@engelen.eu>
2026-07-24 16:11:39 +0200

    docs: improve documentation for version.nix approach (#1454)

--- version.nix
+++ version.nix
@@ -1,12 +1,12 @@
 {
-  /**
-    Backend index version used by import jobs when writing data to Elasticsearch
-  */
+  # Backend index version used by import jobs when writing data to Elasticsearch.
+  #
+  # When making backwards-incompatible schema changes,
+  # change the code for the import job first, updating this version number.
+  # Only after the new index has been populated, update the frontend.
   import = "50";
 
-  /**
-    Frontend index version used by the UI when querying Elasticsearch
-    Keep this at the old version while 'import' populates a new index, then update to switch traffic
-  */
+  # Frontend index version used by the UI when querying Elasticsearch
+  # Keep this at the old version while 'import' populates a new index, then update to switch traffic
   frontend = "50";
 }
